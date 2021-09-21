b11621282d6e05f027020b2910063234f6e06d99
fushaolei <1563250958@qq.com>
2021-09-21 13:43:53 +0800

    update

---
+++ SqStack/cmake-build-debug/CMakeFiles/SqStack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SqStack.dir/main.c.obj"
+  "SqStack.exe"
+  "SqStack.exe.manifest"
+  "SqStack.pdb"
+  "libSqStack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SqStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
