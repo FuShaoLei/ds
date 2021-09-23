11eb27e9252790d3ee20c7af9c680cface757987
fushaolei <1563250958@qq.com>
2021-09-23 17:49:57 +0800

    update

--- StaticLinkList/main.c
+++ StaticLinkList/main.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+#define MAXSIZE 50
+typedef int ElemType;
+typedef int Status;
+
+typedef struct {
+    ElemType data;
+    int cur;
+} Component, StaticLinkList[MAXSIZE];
+
+Status initList(StaticLinkList space) {
+    for (int i = 0; i < MAXSIZE - 1; ++i) {
+        space[i].cur = i + 1;
+    }
+    space[MAXSIZE - 1].cur = 0;
+    return OK;
+}
 
 int main() {
     printf("Hello, World!\n");
