d4a34379a77d357ff1d9b4254aeeffb94eb2d0f7
fushaolei <1563250958@qq.com>
2021-11-10 02:49:18 +0800

    update

--- String/main.c
+++ String/main.c
@@ -7,9 +7,11 @@
 #define OVERFLOW -2
 #define MAXSTRLEN 255
 typedef int Status;
-
 typedef unsigned char SString[MAXSTRLEN + 1]; // 多出一个位置用于存放长度
 
+/**
+ * 初始化
+ */
 Status initStr(SString T, const char *chars) {
     int len = (int) strlen(chars);
     if (len > MAXSTRLEN) {
@@ -25,7 +27,7 @@ Status initStr(SString T, const char *chars) {
 /**
  * 朴素的模式匹配算法
  */
-int simpleMatch(SString S, SString T, int pos) {
+int index_simple(SString S, SString T, int pos) {
     int i = pos;
     int j = 1;
     while (i <= S[0] && j <= T[0]) {
@@ -37,7 +39,7 @@ int simpleMatch(SString S, SString T, int pos) {
             j = 1;
         }
     }
-    if (j > T[0]) return i - T[0];
+    if (j > T[0]) return i - T[0]; // ？
     else return 0;
 }
 
@@ -65,9 +67,31 @@ void get_next(SString T, int next[]) {
 }
 
 /**
+ * 获取nextval数组
+ */
+void get_nextval(SString T, int nextval[]) {
+    int i, j;
+    i = 1;
+    j = 0;
+    nextval[1] = 0;
+    while (i < T[0]) {
+        if (j == 0 || T[i] == T[j]) {
+            ++i;
+            ++j;
+            if (T[i] != T[j])
+                nextval[i] = j;
+            else
+                nextval[i] = nextval[j];
+        } else {
+            j = nextval[j];
+        }
+    }
+}
+
+/**
  * KMP模式匹配算法
  */
-int kmpMatch(SString S, SString T, int pos, int next[]) {
+int index_kmp(SString S, SString T, int pos, int next[]) {
     int i = pos;
     int j = 1;
     while (i <= S[0] && j <= T[0]) {
@@ -82,8 +106,10 @@ int kmpMatch(SString S, SString T, int pos, int next[]) {
     else return 0;
 }
 
-
-void traverString(SString S) {
+/**
+ * 输出打印
+ */
+void printString(SString S) {
     printf("SString = [ ");
     for (int i = 1; i <= S[0]; i++) {
         printf("%c", S[i]);
@@ -94,24 +120,34 @@ void traverString(SString S) {
 
 int main() {
     SString S;
-    char *chars = "ababcabcacbab";
+    char *chars = "goodgoogle";
     initStr(S, chars);
-    traverString(S);
-    SString T;
-    char *tchars = "ababcbb";
-    initStr(T, tchars);
-    traverString(T);
+    printString(S);
 
-    // 测试kmp算法
-    // 1，测试获取next数组
+    SString T;
+    char *tchars = "google";
+    initStr(T, tchars);
+    printString(T);
+
+    // 获取next数组
     int *next = (int *) malloc((T[0] + 1) * sizeof(int));
     get_next(T, next);
-    // 打印测试一下
     printf("next = ");
     for (int p = 1; p <= T[0]; p++) {
         printf("%d", next[p]);
     }
     printf("\n");
+    // 获取nextval数组
+    int *nextval = (int *) malloc((T[0] + 1) * sizeof(int));
+    get_nextval(T, nextval);
+    printf("nextval = ");
+    for (int p = 1; p <= T[0]; p++) {
+        printf("%d", nextval[p]);
+    }
+    printf("\n");
+    // 测试kmp算法
+    int kmp_result = index_kmp(S, T, 1, nextval);
+    printf("kmp_result = %d\n", kmp_result);
 
 
     return 0;
